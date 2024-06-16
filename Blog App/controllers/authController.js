93e196907fcb3d8de2f99516550609df27c65231
CodeWithZain10 <zainzahid10116@gmail.com>
2024-06-17 00:17:47 +0500

    Add Create, Read, Edit & Update Functionality create routes

--- Blog App/controllers/authController.js
+++ Blog App/controllers/authController.js
@@ -3,41 +3,43 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { generateToken } = require("../utils/generateToken");
 
+// Register user
 module.exports.registerUser = async (req, res) => {
   try {
     let { username, name, email, password } = req.body;
+    const profileImg = req.file ? req.file.buffer : null;
     let findUser = await userModel.findOne({ email: email });
     if (findUser) {
       return res
         .status(401)
-        .send("You Already Have An Account, Please Login...");
+        .send("You already have an account. Please log in.");
     } else {
-      bcrypt.genSalt(12, (err, salt) => {
-        bcrypt.hash(password, salt, async (err, hash) => {
-          if (err) return res.send(err);
-          let user = userModel.create({
-            username,
-            name,
-            email,
-            password: hash,
-          });
-          let token = generateToken(user);
-          res.cookie("token", token);
-          res.send("User Created Successfully");
-        });
+      const salt = await bcrypt.genSalt(12);
+      const hash = await bcrypt.hash(password, salt);
+      const user = await userModel.create({
+        profileImg,
+        username,
+        name,
+        email,
+        password: hash,
       });
+      const token = generateToken(user);
+      res.cookie("token", token);
+      res.send("User created successfully");
     }
   } catch (error) {
-    console.log(error.message);
+    console.error(error);
+    res.status(500).send("Server Error");
   }
 };
 
+// Login user
 module.exports.loginUser = async (req, res) => {
   let { email, password } = req.body;
 
   let user = await userModel.findOne({ email });
   if (!user) {
-    res.send("Email & Password is incorrect");
+    req.send("Email & Password is incorrect");
     return res.redirect("/register");
   }
 
@@ -45,14 +47,15 @@ module.exports.loginUser = async (req, res) => {
     if (result) {
       let token = generateToken(user);
       res.cookie("token", token);
-      res.redirect("/");
+      res.render("index");
     } else {
-      res.send("Email & Password is incorrect");
+      req.send("Email & Password is incorrect");
       return res.redirect("/register");
     }
   });
 };
 
+// Logout user
 module.exports.logout = (req, res) => {
   res.cookie("token", "");
   res.redirect("/");
--- Blog App/models/user.js
+++ Blog App/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  profileImg: Buffer,
   username: {
     type: String,
     required: true,
